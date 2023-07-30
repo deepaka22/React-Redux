28eca356300aac453ac2815ca2103f743c7331b6
Deepak Venkatesan <Deepakbakyalakshmi1997@gmail.com>
2023-07-31 01:46:24 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Header from './Components/Header';
-import Section from './Components/Section';
-import Footer from './Components/Footer';
-import Card from './Components/Card';
-import { useState } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
-  const [counter, Setcounter] = useState(0);
-
-  return (
-    <div className="App">
-       
-  <Navbar counter = {counter} Setcounter = {Setcounter}/>
-  <Header />
-  <Section counter = {counter} Setcounter = {Setcounter}/>
-  <Footer/>
-    </div>
-  );
+  return <div className="App">helo</div>;
 }
 
 export default App;
